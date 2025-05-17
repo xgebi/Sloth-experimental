e8741b87819d451038cefcb71f76e8e8a9234cc0
Sarah Gebauer <sarah@sarahgebauer.com>
2025-05-17 16:43:26 +0300

    Added redirect to dashboard as a default

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  async redirects() {
+    return [
+      // Basic redirect
+      {
+        source: '/',
+        destination: '/dashboard',
+        permanent: true,
+      }
+    ]
+  },
+}
 
 export default nextConfig;
